40619f6cd4694e441c20b88d8eb2beda6e7473ff
Haitam Souiri <114674087+aKaALpha21@users.noreply.github.com>
2023-02-01 16:19:48 +0100

    Add files via upload

--- food.js
+++ food.js
@@ -1,55 +1,177 @@
-document.getElementById('select').onchange=function(){
-    var value=document.getElementById('select').value
-    let url1 = "www.themealdb.com/api/json/v1/1/list.php?a=list"
-    let cart = document.getElementById("cards")
-    fetch(url1 + value)
+let urlcategories = "https://www.themealdb.com/api/json/v1/1/list.php?c=list"
+let urlArea = "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
+
+let selectCategorie= document.getElementById("selectCategorie")
+let selectArea= document.getElementById("selectArea")
+let filterBtn = document.getElementById("filter")
+
+
+
+fetch(urlcategories)
+.then((response) => response.json())
+.then((data)=>{
+    let myCategorie = data.meals
+    //    console.log(myCategorie)
+    for (let i in myCategorie){
+        let option = document.createElement("option")
+        option.innerText = `${myCategorie[i].strCategory}`
+        option.setAttribute("value", `${myCategorie[i].strCategory}`)
+        selectCategorie.appendChild(option)
+    }
+    
+    
+})
+fetch(urlArea)
+.then((response) => response.json())
+.then((data)=>{
+    let myArea = data.meals
+    // console.log(myArea)
+    for (let i in myArea){
+        let option = document.createElement("option")
+        option.innerText = `${myArea[i].strArea}`
+        option.setAttribute("value", `${myArea[i].strArea}`)
+        selectArea.appendChild(option)
+    }
+    
+    
+})
+
+
+filterBtn.addEventListener("click", function(){
+    let CategorySelected = document.getElementById("selectCategorie").value    
+    let AreaSelected = document.getElementById("selectArea").value
+    if(CategorySelected!== "null" && AreaSelected !== "null"){
+        filterCategoryAndArea(CategorySelected, AreaSelected)
+    }else if(CategorySelected !== "null"){
+        filterCategory(CategorySelected)
+    }else if(AreaSelected !== "null"){
+        filterArea(AreaSelected)
+    }
+})
+
+
+
+
+
+
+// filter by category
+
+function filterCategory(){
+    let CategorySelected = document.getElementById("selectCategorie").value
+    let cart = document.getElementById("cart")
+    let urlFilterCategorie = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${CategorySelected}`
+
+    
+    fetch(urlFilterCategorie)
     .then((response) => response.json())
     .then((data)=>{
-        let dataFilter = data.meals.filter(meal => meal.strMeal.toLowerCase().includes(value))
-        cart.textContent = ``
-            if(searchValue == "" || searchValue == " "){
-                //display te 6 random
-                display()
-            }else{
-                for(let i in dataFilter){
-                    cart.innerHTML+= 
-                
-                    ` 
+       
+        //to delete the old filter 
+        if(CategorySelected == "null"){
+            dataFilter = ""
+            
+        }else{
+            dataFilter = data.meals.filter(meal => meal.strMeal.toLowerCase())
+        }
+        console.log(CategorySelected)
+        console.log(dataFilter)
+        cart.innerHTML = ""
+        
+        for(let i in dataFilter){
+            
+            cart.innerHTML+= `
+            <div class="cart col-sm-12 col-md-4 col-lg-4"  >
+            <div class="card" style="width: 18rem;" >
+                <img class="card-img-top" src=" ${dataFilter[i].strMealThumb}" >
+                <div class="card-body">
+                <h5 class="card-title">${dataFilter[i].strMeal}</h5>
+               
+                </div>
+            </div>
+            </div>
+         
+           
+            `
+        } 
+
+       
+    })
     
-                    <!-- cards trigger modal -->
-                  <div class="card col-3 container" id="cards${i}">
-                          <img src="${dataFilter[i].strMealThumb}" class="card-img-top" alt="Sunset Over the Sea"/>
-                          <div class="card-body">
-                              <p class="card-text"><span>Name : ${dataFilter[i].strMeal}</span></p>
-                          </div>
-                          <button type="button" class="btn btn-primary" id="btnSearche${i}" data-bs-toggle="modal" data-bs-target="#staticBackdrop${dataFilter[i].idMeal}">
-                          Voir
-                      </button>
-                  </div>
-                  <!-- Modal -->
-                  <div class="modal fade" id="staticBackdrop${dataFilter[i].idMeal}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                      <div class="modal-dialog">
-                      <div class="modal-content">
-                      <div class="modal-header">
-                          <h1 class="modal-title fs-5" id="staticBackdropLabel">Meals</h1>
-                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                      </div>
-                      <div class="modal-body">
-                          <img src="${dataFilter[i].strMealThumb}" class="card-img-top" alt="Sunset Over the Sea"/><br><br>
-                          <p>Name : ${dataFilter[i].strMeal}</p>
-                          <p>Area : ${dataFilter[i].strArea}</p>
-                          <p>recipe : ${dataFilter[i].strInstructions}</p>
-                      </div>
-                      
-                      </div>
-                      </div>
-                  </div>
-                      `;
-                    
-                } 
-                
-            }
-        });
+    
 }
 
 
+//filter by Area
+
+function filterArea(){
+    let AreaSelected = document.getElementById("selectArea").value
+    let cart = document.getElementById("cart")
+    let urlFilterArea = `https://www.themealdb.com/api/json/v1/1/filter.php?a=${AreaSelected}`
+
+    
+    fetch(urlFilterArea)
+    .then((response) => response.json())
+    .then((data)=>{
+       
+        //to delete the old filter 
+        if(AreaSelected == "null"){
+            dataFilter = ""
+            
+        }else{
+            dataFilter = data.meals.filter(meal => meal.strMeal.toLowerCase())
+        }
+        
+        cart.innerHTML = ""
+
+        for(let i in dataFilter){
+            
+            cart.innerHTML+= `
+            <div class="cart col-sm-12 col-md-4 col-lg-4"  >
+            <div class="card" style="width: 18rem;" >
+                <img class="card-img-top" src=" ${dataFilter[i].strMealThumb}" >
+                <div class="card-body">
+                <h5 class="card-title">${dataFilter[i].strMeal}</h5>
+               
+                </div>
+            </div>
+            </div>
+         
+
+            `
+        } 
+
+       
+    })
+    
+    
+}
+async function filterCategoryAndArea(CategorySelected, AreaSelected) {
+    let cart = document.getElementById("cart");
+    let urlFilterCategorie = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${CategorySelected}`;
+    let urlFilterArea = `https://www.themealdb.com/api/json/v1/1/filter.php?a=${AreaSelected}`;
+    let dataFilterCategorie = await fetch(urlFilterCategorie)
+      .then((response) => response.json())
+      .then((data) => data.meals);
+  
+    let dataFilterArea = await fetch(urlFilterArea)
+      .then((response) => response.json())
+      .then((data) => data.meals);
+  
+    let meals = dataFilterCategorie.filter((meal) =>
+      dataFilterArea.some((m) => m.idMeal === meal.idMeal)
+    );
+    cart.innerHTML = "";
+    for (let i in meals) {
+      cart.innerHTML += `
+          <div class="cart col-sm-12 col-md-4 col-lg-4"  >
+          <div class="card" style="width: 18rem;" >
+              <img class="card-img-top" src=" ${meals[i].strMealThumb}" >
+              <div class="card-body">
+              <h5 class="card-title">${meals[i].strMeal}</h5>
+              </div>
+          </div>
+          </div>
+          `;
+    }
+  }
+  
